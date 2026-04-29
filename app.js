1672b3f831d91200534f5e2fecaa6d75f3aabe10
Arpan Das <185752118+arpan-das01@users.noreply.github.com>
2026-04-29 23:57:57 +0530

    feat: add javascript logic to do basic arithmatic and display result on screen

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 const display = document.querySelector(".calculator_display");
 const button = document.querySelectorAll(".button");
-let valueInDisplay=""; // contains string of numbers and operations on display
+const operators = ['+', '-', '*', '/'];
+let valueInDisplay = ""; // contains string of numbers and operations on display
 
-function modifyString(event){ //modifies valueInDisplay 
-    valueInDisplay += event.target.dataset.value;
-    display.textContent=valueInDisplay;
+function modifyString(event){ // modifies valueInDisplay
+    if(event.target.dataset.action === undefined)
+        valueInDisplay += event.target.dataset.value;
+    else if(valueInDisplay === "")
+        return;
+    else if(event.target.dataset.action === "clear")
+        clearDisplay();
+    else if(event.target.dataset.action === '=')
+        calculate();
+    else if(operators.includes(valueInDisplay[valueInDisplay.length - 1])){
+        valueInDisplay = valueInDisplay.slice(0, -1); // prevents double operator inputs
+        valueInDisplay += event.target.dataset.action;
+    }
+    else
+        valueInDisplay += event.target.dataset.action;
+
+    display.textContent = valueInDisplay;
 }
 
 button.forEach(butn => {
     butn.addEventListener('click', modifyString);
 });
+
+function clearDisplay(){
+    valueInDisplay = "";
+}
+
+function calculate(){
+    if(valueInDisplay === "")
+        return;
+    try{
+        let result = eval(valueInDisplay);
+        if(!isFinite(result))
+            valueInDisplay = "Error!";
+        else
+            valueInDisplay = String(result);
+    }catch(error){
+        valueInDisplay = "Syntax Error!"
+    }
+}
